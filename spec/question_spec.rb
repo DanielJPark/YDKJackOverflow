a23eed960f1fcdb8fa6cdd193db3bf8d6af8671a
Gregory Berns-Leone <gberns.l@gmail.com>
2016-08-10 16:27:40 -0500

    Add shoulda matching tests and update config and Gemfile to work

--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe Question do
-  pending "add some examples to (or delete) /Users/Greg/Developer/devbootcamp/2-phase/2-week/Wednesday/YDKJackOverflow/Rakefile"
+describe Question, type: :model do
+  # it { should belong_to :user }
+  it { should have_many :answers }
+  it { should have_many :votes }
+  it { should have_many :comments }
+  # it { should belong_to :category }
 end
+
+describe "testing tests" do
+  it { expect(true).to eq true }
+end
+
