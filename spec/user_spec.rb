32885020a93584c0cb9ee1970176c015e2626f3c
Kaitlyn Bradshaw <kaitlynmbradshaw@Kaitlyns-MacBook-Pro.local>
2016-08-10 16:27:30 -0500

    fix spec tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,6 @@
 require_relative "spec_helper"
 
-describe User do
+describe User, type: :model do
   it { should validate_presence_of :username }
   it { should validate_presence_of :email }
   it { should validate_presence_of :hashed_password }
