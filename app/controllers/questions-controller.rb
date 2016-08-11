d4d6bc8a0fbe6ecdcd3e01ccb815be592dc238fa
Dan Park <email": null>
2016-08-11 13:53:23 -0500

    delete button dynamic to current user... deletes question if no additions.. changes user to anonymous and edits title and content displayed if question has answerss or comments

--- app/controllers/questions-controller.rb
+++ app/controllers/questions-controller.rb
@@ -8,6 +8,10 @@ end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
+  if @question.user == User.find_by(username: "Anonymous")
+    @question.title = "[This Question was deleted by user]"
+    @question.content = "[This Question was deleted by user]"
+  end
   @selected_answer = @question.selected_answer
   @answers = @question.answers.select { |a| a.id != @selected_answer.try(:id) }
   erb :'questions/show'
@@ -15,4 +19,18 @@ end
 
 put '/questions/:id/edit' do
 
+end
+
+
+
+delete '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  if @question.answers.count == 0 && @question.comments.count == 0
+    @question.destroy
+    redirect '/'
+  else
+    @question.user = User.find_or_create_by(username: "Anonymous", email: "anonymous@anonymous.com", hashed_password: "password")
+    @question.save
+    redirect "/questions/#{params[:id]}"
+  end
 end
