b74788c5ab44c2112b3ac34e3d358798d0526574
Dan Park <email": null>
2016-08-10 19:29:23 -0500

    routes fixed

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -27,24 +27,6 @@ get '/logout' do
 end
 
 get '/users/login' do
-<<<<<<< HEAD
-	erb :'users/login'
-end
-
-post '/login' do
-@user = User.find_by(email: params[:email])
-	if @user
-		if @user.authenticate(params[:password])
-	 		session[:user_id] = @user.id
-	 		redirect "/user/show/#{session[:user_id]}"
-		else
-   		erb :'login'
-		end
-	else
-		redirect '/users/new'
-	end
-end
-=======
   @email = session[:username]
   erb :'users/login'
 end
@@ -63,4 +45,3 @@ post '/users/login' do
     erb :'users/login'
   end
 end
->>>>>>> master
