47e18d916e9b486096e811c2d3ae89d58252c5f1
Gregory Berns-Leone <gberns.l@gmail.com>
2016-08-11 14:00:29 -0500

    Add route for posting a vote on an answer

--- app/controllers/questions-controller.rb
+++ app/controllers/questions-controller.rb
@@ -9,10 +9,15 @@ end
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @selected_answer = @question.selected_answer
-  @answers = @question.answers.select { |a| a.id != @selected_answer.id }
+  @answers = @question.answers.select { |a| a.id != @selected_answer.try(:id) }
   erb :'questions/show'
 end
 
 put '/questions/:id/edit' do
 
+end
+
+post '/answers/:id/vote' do
+  answer = Answer.find(params[:id])
+  Vote.create(value: )
 end
