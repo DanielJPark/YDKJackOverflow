af72683aeee689b2c45624f9072a78e695cd9f7f
Kaitlyn Bradshaw <kaitlynmbradshaw@Kaitlyns-MacBook-Pro.local>
2016-08-11 12:30:12 -0500

    missed a comma

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  validates :content, :user_id, :post_id, :post_type { presence: true }
+  validates :content, :user_id, :post_id, :post_type, { presence: true }
 
   belongs_to :user, inverse_of: :comments
   belongs_to :post, polymorphic: true, inverse_of: :comments
