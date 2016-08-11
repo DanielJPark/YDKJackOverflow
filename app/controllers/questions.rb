bd35f71b82e157b374c3c367c6fcc4412dd74142
Dan Park <email": null>
2016-08-11 17:50:28 -0500

    post and delete routes authorization.

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -54,20 +54,28 @@ end
 
 delete '/questions/:id' do
   @question = Question.find_by(id: params[:id])
-  if @question.answers.count == 0 && @question.comments.count == 0
-    @question.destroy
-    redirect '/'
+  if logged_in? && current_user == @question.user
+    if @question.answers.count == 0 && @question.comments.count == 0
+      @question.destroy
+      redirect '/'
+    else
+      @question.user = User.find_or_create_by(username: "Anonymous", email: "anonymous@anonymous.com", hashed_password: "password")
+      @question.save
+      redirect "/questions/#{params[:id]}"
+    end
   else
-    @question.user = User.find_or_create_by(username: "Anonymous", email: "anonymous@anonymous.com", hashed_password: "password")
-    @question.save
-    redirect "/questions/#{params[:id]}"
+    redirect 'users/login'
   end
 end
 
 post '/questions/:id/vote' do
-  question = Question.find(params[:id])
-  Vote.create(value: 1, post: question, user: current_user)
-  redirect "/questions/#{params[:id]}"
+  if logged_in?
+    question = Question.find(params[:id])
+    Vote.create(value: 1, post: question, user: current_user)
+    redirect "/questions/#{params[:id]}"
+  else
+    redirect 'users/login'
+  end
 end
 
 get '/questions/:id/comments/new' do
@@ -79,21 +87,29 @@ get '/questions/:id/comments/new' do
 end
 
 post '/questions/:id/comments' do
-  new_comment = Comment.new(params[:comment])
-  new_comment.user = current_user
-  new_comment.post_type = "Question"
-  new_comment.post_id = params[:id]
-  new_comment.save
+  if logged_in?
+    new_comment = Comment.new(params[:comment])
+    new_comment.user = current_user
+    new_comment.post_type = "Question"
+    new_comment.post_id = params[:id]
+    new_comment.save
 
-  @errors = new_comment.errors.full_messages
+    @errors = new_comment.errors.full_messages
 
-  erb :'questions/newComment' if @errors.length != 0
+    erb :'questions/newComment' if @errors.length != 0
 
-  redirect "/questions/#{params[:id]}"
+    redirect "/questions/#{params[:id]}"
+  else
+    redirect 'users/login'
+  end
 end
 
 delete '/questions/:id/comments/:comment_id' do
   @comment = Comment.find_by(id: params[:comment_id])
-  @comment.destroy
-  redirect "/questions/#{params[:id]}"
+  if logged_in? && current_user == @comment.user
+    @comment.destroy
+    redirect "/questions/#{params[:id]}"
+  else
+    redirect 'users/login'
+  end
 end
