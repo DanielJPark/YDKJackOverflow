4697a349fec5bb46c4801a2d1f1a897b5f90b749
[01;31m[K"name"[m[K: "Laurie Skelly <[01;31m[K"email"[m[K: "laurieskelly@users.noreply.github.com>
2016-08-11 16:09:58 -0500

    tabs to spaces

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,49 +1,49 @@
 get '/categories/:cid/questions/new' do
-	if logged_in?
-		@category = Category.find(params[:cid])
-		@all_categories = Category.order(:title)
-		erb :'/questions/new'
-	else
-		redirect '/users/login'
-	end
+  if logged_in?
+    @category = Category.find(params[:cid])
+    @all_categories = Category.order(:title)
+    erb :'/questions/new'
+  else
+    redirect '/users/login'
+  end
 end
 
 get '/questions/new' do
-	if logged_in?
-		@all_categories = Category.order(:title)
-		erb :'/questions/new'
-	else
-		redirect 'users/login'
-	end
+  if logged_in?
+    @all_categories = Category.order(:title)
+    erb :'/questions/new'
+  else
+    redirect 'users/login'
+  end
 end
 
 post '/questions' do
-	@question = Question.new(params[:question])
-	if @question.save
-		redirect "/questions/#{@question.id}"
-	else
-		@errors = @question.errors.full_messages
-		erb :'/questions/new'
-	end
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = @question.errors.full_messages
+    erb :'/questions/new'
+  end
 end
 
 get '/questions/:id' do
-	@question = Question.find(params[:id])
+  @question = Question.find(params[:id])
 
-	if @question.user == User.find_by(username: "Anonymous")
-		@question.title = "[This Question was deleted by user]"
-		@question.content = "[This Question was deleted by user]"
-	end
+  if @question.user == User.find_by(username: "Anonymous")
+    @question.title = "[This Question was deleted by user]"
+    @question.content = "[This Question was deleted by user]"
+  end
 
-	if @question.selected_answer
-		@selected_answer = @question.selected_answer
-		@answers = @question.answers.select { |a| a.id != @selected_answer.id }
-	else
-		@selected_answer = nil
-		@answers = @question.answers
-	end
+  if @question.selected_answer
+    @selected_answer = @question.selected_answer
+    @answers = @question.answers.select { |a| a.id != @selected_answer.id }
+  else
+    @selected_answer = nil
+    @answers = @question.answers
+  end
 
-	erb :'/questions/show'
+  erb :'/questions/show'
 end
 
 put '/questions/:id/edit' do
